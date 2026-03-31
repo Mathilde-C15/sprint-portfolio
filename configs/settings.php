9941731676077dd7bbcd2ff200e0575eac146965
Mathilde Cotelle <mathildecotelle@orange.fr>
2026-03-31 14:34:25 +0200

    Commit de toutes les créations de fichiers, css pour le header/navigation

---
+++ configs/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+const AVAILABLE_ROUTES = [
+    '/' => [
+        'controller' => 'HomeController',
+        'method' => 'index'
+    ],
+    'login' => [
+        'controller' => 'LoginController',
+        'method' => 'index'
+    ],
+    'project' => [
+        'controller' => 'ProjectController',
+         'method' => 'index'
+    ]
+];
